39ddad487d41cd0dbc74b16d5ce1aa720f4e3528
Cat <jarolika30@gmail.com>
2019-08-28 13:40:20 +0300

    feat: add header component and styles for it

--- commerce/src/App.js
+++ commerce/src/App.js
@@ -1,12 +1,15 @@
 import React from 'react';
 import { Switch, Route } from 'react-router-dom';
 import './App.css';
+
 import Homepage from './pages/homepage/homepage';
 import ShopPage from './pages/shop/shop';
+import Header from './components/header/header';
 
 function App() {
   return (
     <div>
+      <Header />
       <Switch>
         <Route exact path="/" component={Homepage}/>
         <Route path="/shop" component={ShopPage}/>
